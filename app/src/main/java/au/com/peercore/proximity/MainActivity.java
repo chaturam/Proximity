0ebf72fa6df7e5f098cf3e0c43f1fef06cb3a1ca
chaturam <chaturam@ymail.com>
2015-10-13 16:16:17 +1100

    Light sensor

--- app/src/main/java/au/com/peercore/proximity/MainActivity.java
+++ app/src/main/java/au/com/peercore/proximity/MainActivity.java
@@ -1,52 +1,57 @@
 package au.com.peercore.proximity;
 
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    SensorManager sManager;
+    Sensor mSensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = sManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        Log.d("Proximity", "maz: " + mSensor.getMaximumRange());
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sManager.unregisterListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d("Proximity", "Values: " + sensorEvent.values[0]);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        if(sensor.getType() == mSensor.getType()){
+            //Log.d("Proximity", "Accuracy: " + i);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
